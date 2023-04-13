9bd2b2c42a03b8b6a19ab449dbd4635541d07a0b
Hana Lee <leehana@alumni.stanford.edu>
2023-04-13 13:46:41 -0700

    update home page

--- 2024-debuts-app/src/app/top-bar/top-bar.component.ts
+++ 2024-debuts-app/src/app/top-bar/top-bar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  constructor(private router: Router) {}
+
+  navigate(url: string): void {
+    this.router.navigate([url]);
+  }
+}
