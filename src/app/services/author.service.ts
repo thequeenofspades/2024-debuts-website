8f22492f701f44f80862e6c4aac59a29e6c8a526
Hana Lee <leehana@alumni.stanford.edu>
2023-04-13 15:36:32 -0700

    use firestore in authorservice

--- src/app/services/author.service.ts
+++ src/app/services/author.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map, of } from 'rxjs';
 
 import authors from '../authors.json';
 import { Author } from '../types';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorService {
+  db: Firestore = inject(Firestore);
+
   constructor() {}
 
   getAuthorsForDisplay(): Observable<Author[]> {
     // TODO (thequeenofspades): replace with DB call
-    return of(JSON.parse(JSON.stringify(authors)));
+    const authorCollection = collection(this.db, 'authors');
+    return collectionData(authorCollection) as Observable<Author[]>;
   }
 }
